13f6450715f27c6a8329998850796f736d3d6171
markjantonelete <janingxtonelete@gmail.com>
2018-09-04 14:08:37 +0800

    first commit

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,10 +1,5 @@
 app.controller("appController" , ["$scope", "$http", function (state, http){
-	http.get('https://jsonplaceholder.typicode.com/todos')
-	.then(response => response.data)
-	.then(json => state.list = json)
+	http.get('https://newsapi.org/v2/top-headlines?country=ph&apiKey=1d736559caa642f3bcc6076797b42e52')
+		.then(response => response.data)
+		.then(json => state.list = json.articles)
 }])
-
-
-
-
-
