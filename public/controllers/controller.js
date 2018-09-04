a079c1ea07951562312959bdd998c3bd8df61a79
markjantonelete <janingxtonelete@gmail.com>
2018-09-04 15:27:26 +0800

    adding states

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,5 +1,24 @@
 app.controller("appController" , ["$scope", "$http", function (state, http){
-	http.get('https://newsapi.org/v2/top-headlines?country=ph&apiKey=1d736559caa642f3bcc6076797b42e52')
-		.then(response => response.data)
-		.then(json => state.list = json.articles)
+
+	state.category = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
+	state.initialVal = ``
+	state.initialCategory = `general`
+	state.handleSearch = function(){
+		state.initialVal = state.searchval
+		state.initialVal2 = state.initialVal ? `search result for ${state.initialVal}...` : ''
+		fetching()
+	}
+
+	state.handleClick = function(e){
+		state.initialCategory = e
+		fetching()
+	}
+
+	const fetching = () => {
+		http.get(`https://newsapi.org/v2/top-headlines?q=${state.initialVal}&category=${state.initialCategory}&country=ph&apiKey=1d736559caa642f3bcc6076797b42e52`)
+		.then(response => response.data.articles)
+		.then(response => state.list = response)
+	}
+
+	fetching()
 }])
