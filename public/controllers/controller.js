e717c575d22d8b7f4f38ec0982e7ba6e230a5388
markjantonelete <janingxtonelete@gmail.com>
2018-09-04 10:32:57 +0800

    first commit

---
+++ public/controllers/controller.js
@@ -0,0 +1,16 @@
+app.controller("appController" , ["$scope",function($scope){
+	$scope.list = [
+		{
+			"ID":1,
+			"Todo": "Wash Clothes",
+			"Status":"Pending"
+		},
+		{
+			"ID":2,
+			"Todo": "Study",
+			"Status":"Pending"
+		},
+	]
+}]);
+
+
