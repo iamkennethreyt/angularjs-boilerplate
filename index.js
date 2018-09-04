4190bc988907f84d87191d49b1d7934d766dfbe2
markjantonelete <janingxtonelete@gmail.com>
2018-09-04 11:11:02 +0800

    First Commit

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/public'));
 app.get('*', (req,res) => res.sendfile("./public/index.html"));
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log("App listening on port ." + port));
+app.listen(port, () => console.log("App listening on port " + port));
