5a6ff948d9276f1106aa84891cb2906939a29242
Kenneth Rey Tamis <keba.tamis@swu.edu.ph>
2018-09-06 22:09:59 +0800

    change api news from source to articles

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/public'));         
 
 app.get('/api/readapi', (req, res) => {
-    fetch('https://newsapi.org/v2/sources?apiKey=1d736559caa642f3bcc6076797b42e52')
+    fetch('https://newsapi.org/v2/top-headlines?country=ph&apiKey=1d736559caa642f3bcc6076797b42e52')
     .then(response => response.json())
     .then(json => res.send(json))
 });
--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,19 +1,18 @@
 app.controller("appController" , ["$scope", "$http", function (state, http){
 
-	state.sources = []
+	state.articles = []
 	state.category = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
 	state.initialCategory = ``
-	state.numLimit = 10
+	state.numLimit = 5
 
-	state.handleClickReadMore = () => state.numLimit + 5
+	state.handleClickReadMore = () => {
+		state.numLimit = state.numLimit + 5
+	}
 	state.handleClick = (e) => state.initialCategory = e
-	
-	console.log(state.numLimit)
 	
 	state.loader = "fetching data..."
 
 	http.get('/api/readapi')
-		.then((res) => state.sources = res.data.sources)
-		.then((res) => state.loader = "")
-		.catch((err)=> console.log(err))
+		.then((res) => state.articles = res.data.articles)
+		.then(() => state.loader = "")
 }])
