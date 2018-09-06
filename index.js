f87c2722301d2bff85547736a76c944f5dd1337c
Kenneth Rey Tamis <keba.tamis@swu.edu.ph>
2018-09-06 22:29:29 +0800

    change node-fetch to news-api fetching

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const fetch = require('node-fetch')
 const app = express();
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('1d736559caa642f3bcc6076797b42e52');
 
 app.use(express.static(__dirname + '/public'));         
 
 app.get('/api/readapi', (req, res) => {
-    fetch('https://newsapi.org/v2/top-headlines?country=ph&apiKey=1d736559caa642f3bcc6076797b42e52')
-    .then(response => response.json())
+    newsapi.v2.topHeadlines(
+        {
+            language: 'en',
+            country: 'us'
+        }
+    )
     .then(json => res.send(json))
 });
 
 app.get('*', (req,res) => res.sendfile("./public/index.html"));
 
 app.listen(8080, () => console.log(`App listening on port 8080`));
+
+// To query /v2/top-headlines
+// All options passed to topHeadlines are optional, but you need to include at least one of them
